60c5a380798e96e3dd711060c0fb6c22fb2407ba
laosbxd <laosbxd@126.com>
2013-08-10 12:43:50 +0800

    Pre-alpha

--- acehrs6.pas
+++ acehrs6.pas
@@ -3,4 +3,26 @@
  Acehrs V6 Pre-apha
  With MySQL Storage
 --------------------
+Copyright 2008-2013
+L&S Team. 
+KERNEL
+}
+
+const version="v6.0.0 Pre-alpha";
+       ver="v6";
+       db-cfgfile="db.cfg";
+       logfile="acehrs.log"
+
+var db-cfg:text;
+    log:text;
+    
+//Every function has their return value. All returan values are in long integer.
+function log(content:string);
+begin
  
+function init:longint;
+begin
+ write('Welcome! System is initizing...<br/>');
+ write('Starting log...<br/>');
+ assign(log,logfile);
+       
